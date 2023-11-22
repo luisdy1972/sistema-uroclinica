90c92a8e6e1c67e6948d3894914c408ba3cd30f8
Luisdy1972 <luisdy1972@hotmail.com>
2023-11-22 16:41:31 -0500

    add-pages

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,9 @@
-<script setup></script>
+<script setup>
+import { HelloWorld } from '../components'
+</script>
 
 <template>
 	<h2>Home</h2>
+	<HelloWorld msg="Hola Mundo"></HelloWorld>
 </template>
 <style scoped></style>
